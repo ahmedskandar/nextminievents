272d240fba07cf7604664e6bfad7703041ed99a7
Ahmed Lukman <itsahmedlukman@gmail.com>
2022-12-19 15:36:21 +0300

    commit

--- components/pages-components/home-page/Summary.tsx
+++ components/pages-components/home-page/Summary.tsx
@@ -7,7 +7,7 @@ const Summary: FC = () => {
     <section className={classes.section}>
         <div className={classes.container}>
             <div className={classes.text}>
-                Next Events is a website that consists of all technology related events that is about to be undertaken by various speakers.
+                Next Events is a website that consists of all technology related events.
             </div>
             <div>
                 <Image className={classes.image} src={'/images/laptop2.avif'} height={500} width={550} alt={'An image showing a laptop'}/>
